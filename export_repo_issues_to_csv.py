4f984905ecb3b12d37c7a56d4f34913b7136cb84
Mitch Gibbs <git@pmdw.com>
2018-08-31 16:15:08 -0400

    Added comments to the body field if they exist

--- export_repo_issues_to_csv.py
+++ export_repo_issues_to_csv.py
@@ -17,6 +17,32 @@ Supports Github API v3 and ZenHubs current working API.
 Derived from https://gist.github.com/Kebiled/7b035d7518fdfd50d07e2a285aff3977
 """
 
+def get_comments(repo_name, issue_id):
+    comments_for_issue_url  = 'https://api.github.com/repos/%s/issues/%s/comments' % (repo_name,issue_id)
+    r = requests.get(comments_for_issue_url, auth=AUTH)
+    #write_issues(r, FILEOUTPUT, repo_name, repo_ID, starttime)
+    r_json = r.json()
+    #json.dump(r_json, open('data.json', 'w'), indent=4)
+    comment_sum = ''
+    for comment in r_json:
+        comment_sum = comment_sum + str(comment['body'])
+    return comment_sum
+    # more pages? examine the 'link' header returned
+    """ if 'link' in r.headers:
+        pages = dict(
+            [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
+             [link.split(';') for link in
+              r.headers['link'].split(',')]])
+        while 'last' in pages and 'next' in pages:
+            pages = dict(
+                [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
+                 [link.split(';') for link in
+                  r.headers['link'].split(',')]])
+            r = requests.get(pages['next'], auth=AUTH)
+            write_issues(r, FILEOUTPUT, repo_name, repo_ID, starttime)
+            if pages['next'] == pages['last']:
+                break
+    """
 
 def write_issues(r, csvout, repo_name, repo_ID, starttime):
     if not r.status_code == 200:
@@ -28,7 +54,12 @@ def write_issues(r, csvout, repo_name, repo_ID, starttime):
         zenhub_issue_url = 'https://api.zenhub.io/p1/repositories/' + \
             str(repo_ID) + '/issues/' + str(issue['number']) + '?' + ACCESS_TOKEN
         zen_r = requests.get(zenhub_issue_url).json()
-        global Payload
+        #call here to get all comments
+        comments = ''
+        if issue['comments'] > 0:
+            comments = get_comments(repo_name, str(issue['number']))
+
+        global PAYLOAD
 
         if 'pull_request' not in issue:
             global ISSUES
@@ -54,10 +85,14 @@ def write_issues(r, csvout, repo_name, repo_ID, starttime):
             	userstory = markdown.markdown(issue['body'])
             else:
             	userstory = issue['body']
+            	
+            #add the comments to the user story if they exist
+            if issue['comments'] > 0:            
+                userstory = userstory + 'Comments: '+comments
 
             rowvalues =[repo_name, issue['number'], issue['title'], userstory, sPipeline, issue['user']['login'], issue['created_at'],
                              issue['milestone']['title'] if issue['milestone'] else "",issue['milestone']['due_on'] if issue['milestone'] else "",
-                             sAssigneeList[:-1], lEstimateValue, sPhase, sEscDefect,sLabels]
+                             sAssigneeList[:-1], lEstimateValue, sPhase, sEscDefect,sLabels, comments]
 
             for i in range(len(rowvalues)):
                 ws.cell(column=(i+1),row=1+ISSUES,value = rowvalues[i])
@@ -122,7 +157,7 @@ sh = FILEOUTPUT['Sheet']
 FILEOUTPUT.remove(sh)
 
 headers = ['Repository', 'Issue Number', 'Issue Title', 'User Story','Pipeline', 'Issue Author',
-	'Created At', 'Milestone', 'Milestone End Date', 'Assigned To', 'Estimate Value', 'Phase','Escaped Defect','Labels']
+	'Created At', 'Milestone', 'Milestone End Date', 'Assigned To', 'Estimate Value', 'Phase','Escaped Defect','Labels','Comments']
 for i in range(len(headers)):
     ws.cell(column=(i+1),row=1,value = headers[i])
     ws.cell(column=(i+1),row=1).font = Font(bold=True)
