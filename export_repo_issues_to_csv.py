c34712d0456764b96d7687a4a5a01f37725d55c9
Mitch Gibbs <git@pmdw.com>
2019-01-17 21:28:21 -0500

    more pylint

--- export_repo_issues_to_csv.py
+++ export_repo_issues_to_csv.py
@@ -21,131 +21,124 @@ from openpyxl.styles import Font
 
 def get_comments(repo_name, issue_id):
     comments_for_issue_url = f'https://api.github.com/repos/{repo_name}/issues/{issue_id}/comments'
-    r = requests.get(comments_for_issue_url, auth=AUTH)
-    #write_issues(r, FILEOUTPUT, repo_name, repo_ID, starttime)
-    r_json = r.json()
-    #json.dump(r_json, open('data.json', 'w'), indent=4)
+    git_response = requests.get(comments_for_issue_url, auth=AUTH)
+    r_json = git_response.json()
     comment_sum = ''
     for comment in r_json:
         c_login = comment.get("user", dict()).get('login', "")
         comment_sum = '@'+c_login+' - '+comment_sum + str(comment['body'])
     return comment_sum
 
-def write_issues(r, csvout, repo_name, repo_ID):
-    if not r.status_code == 200:
-        raise Exception(r.status_code)
+def write_issues(git_response, repo_name, repo_id):
+    if not git_response.status_code == 200:
+        raise Exception(git_response.status_code)
 
-    r_json = r.json()
+    r_json = git_response.json()
+    zen_url = f'https://api.zenhub.io/p1/repositories/{repo_id}/issues/'
     for issue in r_json:
-        zenhub_issue_url = 'https://api.zenhub.io/p1/repositories/' + \
-            str(repo_ID) + '/issues/' + str(issue['number']) + '?' + ACCESS_TOKEN
-        zen_r = requests.get(zenhub_issue_url).json()
+        issue_number = str(issue['number'])
+        issue_url = f'{zen_url}{issue_number}?{ACCESS_TOKEN}'
+        zen_r = requests.get(issue_url).json()
         #call here to get all comments
         comments = ''
         if issue['comments'] > 0:
-            comments = get_comments(repo_name, str(issue['number']))
+            comments = get_comments(repo_name, issue_number)
 
         global PAYLOAD
 
-        if 'pull_request' not in issue:
-            global ISSUES
-            ISSUES += 1
-            sAssigneeList = ''
-            sPriority = ''
-            sPipeline = ''
-            sLabels = ''
-            for i in issue['assignees'] if issue['assignees'] else []:
-                sAssigneeList += i['login'] + ','
-            for x in issue['labels'] if issue['labels'] else []:
-                sLabels += x['name'] + ','
-                if "Low" in x['name']:
-                    sPriority = x['name']
-                if "Medium" in x['name']:
-                    sPriority = x['name']
-                if "High" in x['name']:
-                    sPriority = x['name']
-             #add output of the payload for records not found
-            sPipeline = zen_r.get("pipeline", dict()).get('name', "")
-            lEstimateValue = zen_r.get("estimate", dict()).get('value', "")
-            if HTMLFLAG == 1:
-                userstory = markdown.markdown(issue['body'])
-            else:
-                userstory = issue['body']
-
-            #add the comments to the user story if they exist
-            if issue['comments'] > 0:
-                userstory = userstory + 'Comments: '+comments
-
-            rowvalues = [repo_name, issue['number'], issue['title'],
-                         userstory, sPipeline, issue['user']['login'], issue['created_at'],
-                         issue['milestone']['title'] if issue['milestone']
-                         else "", issue['milestone']['due_on'] if issue['milestone'] else "",
-                         sAssigneeList[:-1], lEstimateValue, sPriority, sLabels, comments]
-
-            for i in range(len(rowvalues)):
-                ws.cell(column=(i+1), row=1+ISSUES, value=rowvalues[i])
-
-            #Wait added for the ZenHub api rate limit of 100 requests per minute,
-            #wait after the rate limit - 1 issues have been processed
-            if ISSUES%(ZENHUB_API_RATE_LIMIT-1) == 0:
-                print('waiting for API rate limit')
-                time.sleep(45)
+        global ISSUES
+        ISSUES += 1
+        s_assignee_list = ''
+        s_priority = ''
+        s_pipeline = ''
+        s_labels = ''
+        for i in issue['assignees'] if issue['assignees'] else []:
+            s_assignee_list += i['login'] + ','
+        for label in issue['labels'] if issue['labels'] else []:
+            rules = [
+                "Low" in label['name'],
+                "Medium" in label['name'],
+                "High" in label['name'],
+            ]
+            s_labels += label['name'] + ','
+            if any(rules):
+                s_priority = label['name']
+        #add output of the payload for records not found
+        s_pipeline = zen_r.get("pipeline", dict()).get('name', "")
+        estimate_value = zen_r.get("estimate", dict()).get('value', "")
+        if HTMLFLAG == 1:
+            userstory = markdown.markdown(issue['body'])
         else:
-            print(f'You have skipped {ISSUES} Pull Requests')
+            userstory = issue['body']
+        #add the comments to the user story if they exist
+        if issue['comments'] > 0:
+            userstory = userstory + 'Comments: '+comments
+        rowvalues = [repo_name, issue['number'], issue['title'],
+                     userstory, s_pipeline, issue['user']['login'], issue['created_at'],
+                     issue['milestone']['title'] if issue['milestone']
+                     else "", issue['milestone']['due_on'] if issue['milestone'] else "",
+                     s_assignee_list[:-1], estimate_value, s_priority, s_labels, comments]
+        for i in range(len(rowvalues)):
+            WS.cell(column=(i+1), row=1+ISSUES, value=rowvalues[i])
+        #Wait added for the ZenHub api rate limit of 100 requests per minute,
+        #wait after the rate limit - 1 issues have been processed
+        if ISSUES%(ZENHUB_API_RATE_LIMIT-1) == 0:
+            print('waiting for API rate limit')
+            time.sleep(45)
 
 def get_issues(repo_data):
     repo_name = repo_data[0]
-    repo_ID = repo_data[1]
-    issues_for_repo_url = 'https://api.github.com/repos/%s/issues?state=all' % repo_name
-    r = requests.get(issues_for_repo_url, auth=AUTH)
-    write_issues(r, FILEOUTPUT, repo_name, repo_ID)
+    repo_id = repo_data[1]
+    issues_for_repo_url = f'https://api.github.com/repos/{repo_name}/issues?state=all'
+    issue_response = requests.get(issues_for_repo_url, auth=AUTH)
+    write_issues(issue_response, repo_name, repo_id)
     # more pages? examine the 'link' header returned
-    if 'link' in r.headers:
+    if 'link' in issue_response.headers:
         pages = dict(
             [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
              [link.split(';') for link in
-              r.headers['link'].split(',')]])
+              issue_response.headers['link'].split(',')]])
         while 'last' in pages and 'next' in pages:
             pages = dict(
                 [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
                  [link.split(';') for link in
-                  r.headers['link'].split(',')]])
-            r = requests.get(pages['next'], auth=AUTH)
-            write_issues(r, FILEOUTPUT, repo_name, repo_ID)
+                  issue_response.headers['link'].split(',')]])
+            issue_response = requests.get(pages['next'], auth=AUTH)
+            write_issues(issue_response, repo_name, repo_id)
             if pages['next'] == pages['last']:
                 break
 
 
 PAYLOAD = ""
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file_name', help='file_name=filename.txt')
-parser.add_argument('--repo_list', nargs='+', help='repo_list owner/repo zenhub-id')
-parser.add_argument('--html', default=0, type=int, help='html=1')
-args = parser.parse_args()
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('--file_name', help='file_name=filename.txt')
+PARSER.add_argument('--repo_list', nargs='+', help='repo_list owner/repo zenhub-id')
+PARSER.add_argument('--html', default=0, type=int, help='html=1')
+ARGS = PARSER.parse_args()
 
-REPO_LIST = args.repo_list
+REPO_LIST = ARGS.repo_list
 AUTH = ('token', os.environ['AUTH'])
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ZENHUB_API_RATE_LIMIT = 100
 
 TXTOUT = open('data.json', 'w')
 ISSUES = 0
-FILENAME = args.file_name
-HTMLFLAG = args.html
+FILENAME = ARGS.file_name
+HTMLFLAG = ARGS.html
 
 FILEOUTPUT = Workbook()
 
-ws = FILEOUTPUT.create_sheet(title="Data")
-sh = FILEOUTPUT['Sheet']
-FILEOUTPUT.remove(sh)
+WS = FILEOUTPUT.create_sheet(title="Data")
+SH = FILEOUTPUT['Sheet']
+FILEOUTPUT.remove(SH)
 
-headers = ['Repository', 'Issue Number', 'Issue Title', 'User Story', 'Pipeline', 'Issue Author',
+HEADERS = ['Repository', 'Issue Number', 'Issue Title', 'User Story', 'Pipeline', 'Issue Author',
            'Created At', 'Milestone', 'Milestone End Date', 'Assigned To',
            'Estimate Value', 'Priority', 'Labels', 'Comments']
-for h in range(len(headers)):
-    ws.cell(column=(h+1), row=1, value=headers[h])
-    ws.cell(column=(h+1), row=1).font = Font(bold=True)
+for h in range(len(HEADERS)):
+    WS.cell(column=(h+1), row=1, value=HEADERS[h])
+    WS.cell(column=(h+1), row=1).font = Font(bold=True)
 
 #for repo_data in REPO_LIST:
 get_issues(REPO_LIST)
