e56c928719155acc6e503cb5867fca98cba2de1e
migibbs <git@pmdw.com>
2020-08-26 21:32:24 -0400

    added status for Deployed

--- export_repo_issues_to_csv.py
+++ export_repo_issues_to_csv.py
@@ -90,18 +90,9 @@ def create_blocked_items(issues):
             )
     #find all issues to ignore
     for issue in issues:
-        if issue['state'] == 'closed':
-            milestone = '2200-01-01T23:59:59Z'
-        elif issue['milestone']:
-            milestone = issue['milestone'].get('due_on', '2200-01-01T23:59:59Z')
-        else:
-            milestone = '2200-01-01T23:59:59Z'
         if issue['number'] in fix_blockers:
-            #print(f"Do not add {issue['number']} to closed...")
             continue
-        #if the issue is not due within this sprint or earlier, ignore dependencies
-        if datetime.datetime.strptime(milestone, '%Y-%m-%dT%H:%M:%SZ') \
-           < datetime.datetime.now():# + datetime.timedelta(days=14):
+        if issue['state'] == 'closed':
             closed[issue['number']] = AttrDict(
                 issue_number=issue['number']
             )
@@ -289,7 +280,8 @@ def calculate_status(issue):
     now = datetime.datetime.now().replace(microsecond=0)
 
     if issue['state'] == 'closed':
-        milestone = '2200-01-01T23:59:59Z'
+        status = 'Deployed'
+        return status
     elif issue['milestone']:
         milestone = issue['milestone'].get('due_on', now.strftime('%Y-%m-%dT%H:%M:%SZ'))
     else:
